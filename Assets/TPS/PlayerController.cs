e46f5c69397da1f08f95242a736c2d43b9e5968d
yosasito <sato3104y@gmail.com>
2025-10-28 16:56:45 +0900

    プレイヤーの挙動変更

--- Assets/TPS/PlayerController.cs
+++ Assets/TPS/PlayerController.cs
@@ -2,45 +2,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float speed=15.0f;
+    [SerializeField] float speed = 15.0f;
     [SerializeField] float dash = 25f;
     [SerializeField] public float sutamina = 25f;
 
-    Rigidbody rb;
-    Vector3 move;
-
     public bool dead = false;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Rigidbody rb;
+    Vector3 move;
+    Quaternion m_Rotation = Quaternion.identity;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
     void Update()
     {
         float v = Input.GetAxis("Vertical");
         float h = Input.GetAxis("Horizontal");
 
+        move = new Vector3(h, 0, v);
+
+        // 向きの更新（移動方向に回転）
+        if (move.sqrMagnitude > 0.001f)
+        {
+            // 入力方向を基に回転を設定
+            m_Rotation = Quaternion.LookRotation(move);
+        }
+        // ダッシュとスタミナ処理
         if (Input.GetKey(KeyCode.Space))
         {
             if (sutamina > 0)
             {
-                move = new Vector3(h, 0, v) * dash;
-                Debug.Log("dash");
+                move *= dash;
                 sutamina -= 0.01f;
-            }        
-            else if (sutamina <= 0)
+            }
+            else
             {
-                move = new Vector3(h, 0, v) * speed;
+                move *= speed;
             }
         }
-
         else
         {
-            move = new Vector3(h, 0, v) * speed;
-            Debug.Log("walk");
+            move *= speed;
             sutamina += 0.003f;
             if (sutamina >= 25f)
             {
@@ -50,9 +54,12 @@ public class PlayerController : MonoBehaviour
     }
     void FixedUpdate()
     {
+        // 回転を適用
+        rb.MoveRotation(m_Rotation);
+        // 速度を直接適用
         rb.linearVelocity = new Vector3(move.x, rb.linearVelocity.y, move.z);
     }
-    public void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
