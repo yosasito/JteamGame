7982b9d231d40dd10259373f33631fb6de0edac1
yosasito <sato3104y@gmail.com>
2025-11-11 15:45:34 +0900

    table追加,チェスをItem化

--- Assets/TPS/KeyScript.cs
+++ Assets/TPS/KeyScript.cs
@@ -6,6 +6,7 @@ public class KeyScript : MonoBehaviour
     public int point = 1;
 
     Got_Item getKey;
+    [SerializeField] float floatHeight = 0.2f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,5 +24,17 @@ public class KeyScript : MonoBehaviour
             //point++;
             //Debug.Log("score=" + point);
         }
+    }
+
+     void Update()
+    {
+        Quaternion a = Quaternion.AngleAxis(15 * Time.deltaTime, Vector3.up);
+        Quaternion b = transform.rotation;
+
+        transform.rotation = a * b;
+
+        float y = Mathf.Sin(Time.time * 2f) * floatHeight;
+        transform.position = new Vector3(transform.position.x, transform.position.y + (y * Time.deltaTime), transform.position.z);
+
     }
 }
