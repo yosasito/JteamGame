4003ceee3296f877dd3959fac8f7563b31038c07
Almia-magdalene <g2427084@st.t-kougei.ac.jp>
2025-11-11 18:15:52 +0900

    収集必須

--- Assets/PlayerSample.cs
+++ Assets/PlayerSample.cs
@@ -2,11 +2,33 @@ using UnityEngine;
 
 public class PlayerSample : MonoBehaviour
 {
+    [SerializeField] private int targetCount = 3; // 必要なキー数
+    private int currentCount = 0; // 現在の取得数
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Key")
+        // Keyタグのオブジェクトにぶつかったら
+        if (collision.gameObject.CompareTag("Key"))
+        {
+            CollectItem(collision.gameObject);
+        }
+    }
+
+    private void CollectItem(GameObject item)
+    {
+        currentCount++;
+
+        // アイテムを消す（触ったキーを削除）
+        Destroy(item);
+
+        Debug.Log("キーを" + currentCount + "個集めた！");
+
+        // 必要数に達したらタグを変更
+        if (currentCount >= targetCount)
         {
             this.tag = "KeyHave";
+            Debug.Log("タグがKeyHaveに変わった！");
         }
     }
 }
+
