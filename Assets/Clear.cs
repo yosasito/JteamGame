a6daded1442713bf940c80ef9513e5cd9c3dbda6
Almia-magdalene <g2427084@st.t-kougei.ac.jp>
2025-12-09 17:43:50 +0900

    Clear

--- Assets/Clear.cs
+++ Assets/Clear.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Clear : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void OnCollisionEnter(Collision collision)
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("ぶつかった相手: " + collision.gameObject.name);
+
+        if (collision.gameObject.CompareTag("Goal"))
+        {
+            Debug.Log("Goalタグを検出！");
+            SceneManager.LoadScene("Finish");
+        }
     }
 }
