500478299795a01edefe2eeb78d1e406628872be
Almia-magdalene <g2427084@st.t-kougei.ac.jp>
2026-01-05 17:54:40 +0900

    Update DeadCheck.cs

--- Assets/TpsScripts/DeadCheck.cs
+++ Assets/TpsScripts/DeadCheck.cs
@@ -33,7 +33,7 @@ public class DeadCheck : MonoBehaviour
 
     private IEnumerator GoToGameOver()
     {
-        yield return new WaitForSeconds(10f);
+        yield return new WaitForSeconds(1f);
         SceneManager.LoadScene("GameOver");
     }
 }
